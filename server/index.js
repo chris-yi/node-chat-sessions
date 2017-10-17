03188ccca74e5a8a705c9aef611e51c370684c18
chris-yi <kri5to.yi@gmail.com>
2017-10-17 14:21:36 -0600

    reference

--- server/index.js
+++ server/index.js
@@ -1,17 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `${__dirname}/controllers/messages_controller` );
+const session = require("express-session");
+const createInitialSession = require("./middlewares/session")
+const filter = require("./middlewares/filter")
 
 const app = express();
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../public/build` ) );
+app.use(session({
+    secret: "akldjfa;lsdkjfa;dkfja;sdkfja;dkj",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 10000
+    }
+}))
+app.use(createInitialSession)
+app.use(() => {
+    if(req.method === "POST" || req.method === "PUT") {
+        filter(req, res, next);
+    }
+    next();
+})
+
 
 const messagesBaseUrl = "/api/messages";
 app.post( messagesBaseUrl, mc.create );
 app.get( messagesBaseUrl, mc.read );
 app.put( `${messagesBaseUrl}`, mc.update );
 app.delete( `${messagesBaseUrl}`, mc.delete );
+app.get("/api/messages/history", mc.history );
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
